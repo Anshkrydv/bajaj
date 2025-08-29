4cd4ae0716bfd00d6a2ea704ae3b64036d59f8ad
Anshkrydv <anshkumar5201@gmail.com>
2025-08-29 11:42:11 +0530

    fix

--- src/main/java/com/bajaj/WebhookSolverApplication.java
+++ src/main/java/com/bajaj/WebhookSolverApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import com.bajaj.service.WebhookService;
 @SpringBootApplication
 public class WebhookSolverApplication {
 
